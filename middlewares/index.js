b2b0c42e3b6e3c1d56a5ef1af297358c16ca4064
MxzGnTh <mixzio.gonzalez@gmail.com>
2022-04-12 21:27:05 -0400

    commit heroku app corregido

--- middlewares/index.js
+++ middlewares/index.js
@@ -10,24 +10,25 @@ exports.load = (app) => {
 	app.use(
 		express.static(path.join(__dirname, "../node_modules/bootstrap/dist"))
 	);
+    app.use(
+        expressFileUpload({
+            limits: { fileSize: 50 * 1024 * 1024 }, //5mb
+            abortOnLimit: true,
+            responseOnLimit: "el peso máximo es de 5MB",
+        })
+    );
+    app.use(express.static(__dirname + "/public"));
+    app.post("/", (req, res) => {
+        const { foto } = req.files;
+        console.log(foto);
+        if (req.files.foto.size >= 50 * 1024 * 1024) {
+            return res.send("solo hasta 5MB");
+        }
+        const mimeTypes = ["image/jpeg", "image/png"];
+        if (!mimeTypes.includes(foto.mimetype)) {
+            return res.send("solo png o jpg");
+        }
+    });
 };
 
-app.use(
-	expressFileUpload({
-		limits: { fileSize: 50 * 1024 * 1024 }, //5mb
-		abortOnLimit: true,
-		responseOnLimit: "el peso máximo es de 5MB",
-	})
-);
-app.use(express.static(__dirname + "/public"));
-app.post("/", (req, res) => {
-	const { foto } = req.files;
-	console.log(foto);
-	if (req.files.foto.size >= 50 * 1024 * 1024) {
-		return res.send("solo hasta 5MB");
-	}
-	const mimeTypes = ["image/jpeg", "image/png"];
-	if (!mimeTypes.includes(foto.mimetype)) {
-		return res.send("solo png o jpg");
-	}
-});
+
